01aba9e90ad44d17619284338d314fb51a29190c
Dylan Jack <48197729+Dylan4Jacks@users.noreply.github.com>
2021-06-09 17:53:28 +1000

    mounting of css and rendering HTMLResponse success

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import requests
 import json
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from requests.auth import HTTPBasicAuth
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
+app = FastAPI()
 
+#html directory
+app.mount("/public", StaticFiles(directory="public"), name="public")
+templates = Jinja2Templates(directory="public/views")
+
+#URL for the external API
 url = "https://api.languageconfidence.ai/pronunciation-trial-V2/score"
 
+#audio sample
 with open('test_audio.txt', 'r') as file:
     audio_data = file.read().replace('\n', '')
 
@@ -36,9 +43,16 @@ response_json = {
 		}
 
 
-app = FastAPI()
-
 @app.get("/")
 async def root():
     return response_json
+
+
+@app.get("/index", response_class=HTMLResponse)
+async def write_index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/html_render/{id}", response_class=HTMLResponse)
+async def write_home(request: Request, id: int):
+    return templates.TemplateResponse("home.html", {"request": request, "id": id })
 
