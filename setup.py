2075b0cef794485f1f0a681a19d160eb8b0d7d58
Dylan Jack <79838810+DylanJackUni@users.noreply.github.com>
2021-06-09 12:00:17 +1000

    setup.py added

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Learning_Confidence_API_Integration',
+      version='1.0',
+      description='Project which links my own FastAPI with Language Confidences API',
+      author='Dylan Jack',
+      author_email='',
+      
+     )
