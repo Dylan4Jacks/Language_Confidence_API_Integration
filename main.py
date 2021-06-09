3d85b074873f0464b43c8c1e00554fa07312ac09
Dylan Jack <79838810+DylanJackUni@users.noreply.github.com>
2021-06-09 14:03:00 +1000

    added html front and API connection with Key authentication

--- main.py
+++ main.py
@@ -1,10 +1,43 @@
 from fastapi import FastAPI
+from requests.auth import HTTPBasicAuth
+import requests
+import json
+
+url = "https://api.languageconfidence.ai/pronunciation-trial-V2/score"
+
+with open('test_audio.txt', 'r') as file:
+    audio_data = file.read().replace('\n', '')
+
+
+payload = json.dumps({
+  "format": "wav",
+  "content": "I really like green apples",
+  "audioBase64": audio_data
+})
+headers = {
+  'Content-Type': 'application/json',
+  'x-api-key': 'gr6UIed5a6O0Ldj1d6sR9Jmq3j6kR2e4GN2ozK5d'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+response_json_unprocessed = json.loads(response.text)
+
+scores = []
+for word in response_json_unprocessed["word_list"]:
+	scores.append(word['mean'])
+response_json = {
+		'scores' : scores,
+		'av_score' : response_json_unprocessed['sentence_mean'],
+		}
+
 
 app = FastAPI()
 
-#domain where this api is hosted for example : localhost:5000/docs to see swagger documentation automagically generated.
-
-
 @app.get("/")
-def home():
-    return {"message":"Hello TutLinks.com"}
+async def root():
+    return response_json
+
+@app.get("/items/{item_id}")
+async def read_item(item_id):
+    return {"item_id": item_id}
